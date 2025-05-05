9a34aad0b20ee7f87de4a114d668cf0b978f81cc
eduardafs7 <eduarda.freire@a.ucb.br>
2025-05-04 23:14:04 -0300

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float valorMercadoria, total = 0, media;
+    int contador = 0;
+    char resposta;
+
+    do {
+        printf("Digite o valor da mercadoria: ");
+        scanf("%f", &valorMercadoria);
+        total += valorMercadoria;
+        contador++;
+
+        printf("MAIS MERCADORIAS (S/N)? ");
+        scanf(" %c", &resposta); // Espaço antes para limpar o buffer de entrada
+    } while (resposta == 'S' || resposta == 's');
+
+    media = total / contador;
+    printf("O valor total das mercadorias é: R$ %.2f\n", total);
+    printf("A média de valor das mercadorias é: R$ %.2f\n", media);
+
+    return 0;
+}
